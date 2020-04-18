8e4205d0b9e28130b6074cc207aa2b71a1857c30
WWW5911 <56112100+WWW5911@users.noreply.github.com>
2020-04-18 16:02:37 +0800

    12

--- js/Modal_PA.js
+++ js/Modal_PA.js
@@ -22,6 +22,9 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+	if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
 }
 
 // Get the modal
@@ -41,11 +44,4 @@ btn2.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span2.onclick = function() {
   modal2.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
-  }
 }
